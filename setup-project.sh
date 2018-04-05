254003ce7998698d657743f7bb2271589ad139bf
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-04-05 17:16:36 +0200

    switch to oc project if already existent

--- setup-project.sh
+++ setup-project.sh
@@ -1,4 +1,4 @@
-oc new-project xwiki
+oc new-project xwiki || oc project xwiki
 sudo oc login -u system:admin
 sudo oc adm policy add-scc-to-user anyuid -z default -n xwiki
 oc new-app -f secret-template.yaml
